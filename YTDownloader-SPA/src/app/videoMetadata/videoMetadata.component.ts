0a4118a3a88a33bf8aa4a3a6951e35da3bfac84d
Kamil Kosobudzki <kamilkosobudzki1@gmail.com>
2019-12-27 14:14:33 +0100

    Added radio buttons with video qualities although still need to sort video qualities cuz they're out of order, added video thumbnail and title appearing after parsing. There's only downloading and layout to be made

--- YTDownloader-SPA/src/app/videoMetadata/videoMetadata.component.ts
+++ YTDownloader-SPA/src/app/videoMetadata/videoMetadata.component.ts
@@ -10,15 +10,20 @@ export class VideoMetadataComponent implements OnInit {
 
   videoMetadata: any;
   videoUrl: string;
+  selectedQuality: string;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() { }
 
+  onQualitySubmit() {
+    console.log(this.selectedQuality);
+  }
+
   getMetadata() {
     let params = new HttpParams();
     params = params.append('videoUrl', this.videoUrl);
-    this.http.get('http://localhost:5000/VideoDownload/GetVideoMetaData',{params})
+    this.http.get('http://localhost:5000/VideoDownload/GetVideoMetaData', {params})
     .subscribe(response => {
         this.videoMetadata = response;
     },
